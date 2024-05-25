f2ac2435b1ef74575881856e13d2f001bff08bc5
eawetchy <10432999+eawetchy@users.noreply.github.com>
2024-05-25 13:29:37 +0100

    Enable 2-way conversation, add console app file for testing

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
-from discord import Client, Intents, Interaction, Object, app_commands
+"""Discord bot to talk to an LLM."""
+from discord import Client, Intents, Interaction, Object, app_commands, DMChannel
 import os
 import requests
 
-MY_GUILD = Object(1237341194338570250)
+# MY_GUILD = Object(1237341194338570250)
+MY_GUILD = Object(1243870532407922740)  # development server
 
 
 class MyClient(Client):
     def __init__(self, *, intents: Intents):
-        super().__init__(intents=intents)
+        super().__init__(command_prefix='!', intents=intents)
         # A CommandTree is a special type that holds all the application command
         # state required to make it work. This is a separate class because it
         # allows all the extra state to be opt-in.
@@ -25,6 +27,38 @@ class MyClient(Client):
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
+    async def on_ready(self):
+        print("Logged in as {0.user}".format(client))
+
+    # on message in user DM:
+    # read user messages and surround with [INST] [/INST]
+    # read system messages and append </s>
+    # generate next response
+    async def on_message(self, message):
+        if message.content == "!clear" and isinstance(message.channel, DMChannel):
+            print("Clearing bot messages from DM history")
+            channel = message.channel
+            messages = [msg async for msg in channel.history(limit=200)]
+            for msg in messages:
+                if msg.author.id == self.user.id:
+                    print("deleting", msg)
+                    await msg.delete()
+        if (message.author == self.user or
+                not isinstance(message.channel, DMChannel)):
+            # Abort if the message was sent by this bot, or isn't in a DM channel
+            return
+        prompt = start_prompt
+        channel = message.channel
+        async with channel.typing():
+            messages = [message async for message in channel.history(limit=200)]
+            for message in reversed(messages):
+                if message.author == self.user:
+                    prompt += f"{message.content}</s>"
+                else:
+                    prompt += f"[INST]{message.content}[/INST]"
+            next_message = get_llm_message(prompt)
+        await channel.send(next_message)
+
 
 intents = Intents(messages=True)
 client = MyClient(intents=intents)
@@ -40,12 +74,17 @@ def query(payload):
     return response.json()
 
 
-def get_first_llm_message():
+start_prompt = """[INST]You are a study advisor. Your task is to ask students to describe their study processes. Reply 
+               with a friendly greeting and a single question asking to describe their favourite study method.[/INST]"""
+message_context = start_prompt
+
+
+def get_llm_message(prompt):
     output = query({
-        "inputs": """<s>[INST]You are a study advisor. Your task is to ask students to describe their study processes. Reply 
-               with a friendly greeting and a single question asking to describe their favourite study method.[/INST]""",
+        "inputs": prompt
     })
-    return output[0]["generated_text"].split("[/INST]")[1]
+    generated_response = output[0]["generated_text"].split("[/INST]")[-1]
+    return generated_response
 
 
 @client.tree.command()
@@ -56,13 +95,8 @@ async def studybot(interaction: Interaction):
     user_channel = await interaction.user.create_dm()
     await user_channel.send(f'Starting conversation...')
     async with user_channel.typing():
-        llm_message = get_first_llm_message()
+        llm_message = get_llm_message(start_prompt)
     await user_channel.send(llm_message)
-
-
-@client.event
-async def on_ready():
-    print("Logged in as {0.user}".format(client))
 
 
 client.run(token)
