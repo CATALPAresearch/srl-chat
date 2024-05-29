e7df29eadf527285022c95875b519f8c0dc703e6
eawetchy <10432999+eawetchy@users.noreply.github.com>
2024-05-29 19:53:56 +0100

    Added language switch functionality, changed model to better support German

--- bot.py
+++ bot.py
@@ -6,8 +6,29 @@ import requests
 # MY_GUILD = Object(1237341194338570250)
 MY_GUILD = Object(1243870532407922740)  # development server
 
+translations = {
+    "language_not_supported_message": """The language you have specified is currently not supported. Please specify 'en' for English or 'de' for German.
+Die von dir gewünschte Sprache wird derzeit nicht unterstützt. Bitte gib entweder 'en' für Englisch oder 'de' für Deutsch an.""",
+    "en": {
+        "command_response": "Please check your DMs for a message from me.",
+        "conversation_start": "Starting conversation...",
+        "start_prompt": """[INST]You are a study advisor. Your task is to ask students to describe their study processes. Reply \
+with a friendly greeting and a single question asking to describe their favourite study method.[/INST]"""
+    },
+    "de": {
+        "command_response":  "Du bekommst gleich eine persönliche Nachricht von mir.",
+        "conversation_start": "Unser Gespräch beginnt gleich...",
+        "start_prompt": """[INST]Du bist ein Studienberater an einer deutschen Universität. Deine Aufgabe ist es, Studenten \
+und Studentinnen nach ihren bevorzugten Lernmethoden zu fragen. Beginne das Gespräch mit einer \
+freundlichen Begrüßung und einer Aufforderung, eine Lernmethode zu beschreiben, die die Person selbst \
+oft einsetzt. Antworte stets auf Deutsch.[/INST]"""
+    }
+}
+
 
 class MyClient(Client):
+    language = ""
+
     def __init__(self, *, intents: Intents):
         super().__init__(command_prefix='!', intents=intents)
         # A CommandTree is a special type that holds all the application command
@@ -43,11 +64,12 @@ class MyClient(Client):
                 if msg.author.id == self.user.id:
                     print("deleting", msg)
                     await msg.delete()
+            return
         if (message.author == self.user or
                 not isinstance(message.channel, DMChannel)):
             # Abort if the message was sent by this bot, or isn't in a DM channel
             return
-        prompt = start_prompt
+        prompt = translations[client.language]["start_prompt"]
         channel = message.channel
         async with channel.typing():
             messages = [message async for message in channel.history(limit=200)]
@@ -65,18 +87,13 @@ client = MyClient(intents=intents)
 
 token = os.environ['BOT_TOKEN']
 
-API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
+API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
 headers = {"Authorization": f"Bearer {os.environ['HF_TOKEN']}"}
 
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
-
-
-start_prompt = """[INST]You are a study advisor. Your task is to ask students to describe their study processes. Reply 
-               with a friendly greeting and a single question asking to describe their favourite study method.[/INST]"""
-message_context = start_prompt
 
 
 def get_llm_message(prompt):
@@ -87,16 +104,33 @@ def get_llm_message(prompt):
     return generated_response
 
 
-@client.tree.command()
-async def studybot(interaction: Interaction):
+@client.tree.command(name="studybot")
+@app_commands.describe(
+    language="'en' for English | 'de' für Deutsch",
+)
+async def studybot(interaction: Interaction, language: str):
     """Start a conversation with StudyBot!"""
-    await interaction.response.send_message(f'Hi, {interaction.user.mention}! ' +
-                                            'Please check your DMs for a message from me.')
+    if language != "en" and language != "de":
+        await interaction.response.send_message(translations["language_not_supported_message"])
+        return
+
+    client.language = language
+
+    await interaction.response.send_message(f"Hey {interaction.user.mention}! {translations[language]['command_response']}")
     user_channel = await interaction.user.create_dm()
-    await user_channel.send(f'Starting conversation...')
+    await user_channel.send(translations[language]['conversation_start'])
     async with user_channel.typing():
-        llm_message = get_llm_message(start_prompt)
+        llm_message = get_llm_message(translations[language]["start_prompt"])
     await user_channel.send(llm_message)
+
+
+@client.tree.command(name='sync', description='Owner only')
+async def sync(interaction: Interaction):
+    if interaction.user.id == 565256380727164949:
+        await client.tree.sync()
+        await interaction.response.send_message('Command tree synced.')
+    else:
+        await interaction.response.send_message('You must be the owner to use this command!')
 
 
 client.run(token)
