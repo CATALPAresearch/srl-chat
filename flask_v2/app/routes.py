5a4248aa2a139fc23b37c9e6601b2395517afe8c
eawetchy <10432999+eawetchy@users.noreply.github.com>
2024-06-12 21:38:38 +0100

    Recreate start and reply endpoints using ORM

--- flask_v2/app/routes.py
+++ flask_v2/app/routes.py
@@ -1,7 +1,13 @@
 from app import app, db
-from app.models import User
+from app.models import User, Language
 
+from flask import request
+import json
 import sqlalchemy as sa
+from xml.sax.saxutils import escape as xmlescape
+from html import escape as htmlescape
+
+from app.llm import get_llm_message
 
 
 @app.route('/')
@@ -17,3 +23,70 @@ def get_user(id):
     if user is None:
         return "User not found", 404
     return user.client
+
+
+@app.route("/startConversation", methods=['POST'])
+def start_conversation():
+    """
+    Post request format:
+    {
+        "language": "en" or "de",
+        "client": "discord",
+        "userid": Discord user ID
+    }
+    """
+    with open("C:/Users/Elisabeth/Uni/StudyBotPy/flask_v2/translations.json") as file:
+        translations = json.load(file)
+    content = request.json
+    language = content["language"]
+    client = content["client"]
+    userid = content["userid"]
+
+    if language != "en" and language != "de":
+        msg = translations["language_not_supported_message"]
+        response = xmlescape(msg, {"ä": "&auml;", "ö": "&ouml;", "ü": "&uuml;"})
+        return htmlescape(response), 400
+
+    start_prompt = translations["translations"][language]["start_prompt"]["text"]
+    llm_message = get_llm_message("mixtral", start_prompt)
+
+    language_db = db.session.scalar(
+        sa.select(Language).where(Language.lang_code == language))
+    user = User(id=userid, client=client, language_id=language_db.id,
+                message_history=f"<s>[INST]{start_prompt}[/INST]{llm_message}</s>")
+    db.session.add(user)
+    db.session.commit()
+    created_user = db.session.scalar(
+        sa.select(User).where(User.id == userid and User.client == client))
+    if created_user is None:
+        return "An error occurred, please try to restart the conversation", 500
+    # TODO: check if user has already started a conversation so we continue from there
+    return llm_message
+
+
+@app.route("/reply", methods=['POST'])
+def reply():
+    """
+    Post request format:
+    {
+        "client": "discord",
+        "userid": Discord user ID
+        "message": message
+    }
+    """
+    # TODO error if conversation not started - redirect?
+    content = request.json
+    client = content["client"]
+    userid = content["userid"]
+    user_message = content["message"]
+
+    user = db.session.scalar(
+        sa.select(User).where(User.id == userid and User.client == client))
+    message_history = user.message_history
+    prompt = f"{message_history}[INST]{user_message}[/INST]"
+
+    llm_message = get_llm_message("mixtral", prompt)
+
+    user.message_history += f"[INST]{user_message}[/INST]{llm_message}"
+    db.session.commit()
+    return llm_message
