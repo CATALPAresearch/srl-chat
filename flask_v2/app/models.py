2d627ed6c46d695d179713acc34579f4ceed34bd
eawetchy <10432999+eawetchy@users.noreply.github.com>
2024-07-07 17:50:17 +0100

    Store and track remaining learning contexts until interview is complete

--- flask_v2/app/models.py
+++ flask_v2/app/models.py
@@ -18,8 +18,15 @@ class User(db.Model):
     client: so.Mapped[str] = so.mapped_column(sa.String(64), primary_key=True)
     language_id: so.Mapped[str] = so.mapped_column(sa.ForeignKey(Language.id))
     message_history: so.Mapped[Optional[str]] = so.mapped_column(sa.String())
-    interview_answers: so.Mapped[List["InterviewAnswer"]] = so.relationship(back_populates="user")
-    conversation_state: so.Mapped["ConversationState"] = so.relationship(back_populates="user")
+    interview_answers: so.Mapped[List["InterviewAnswer"]] = so.relationship(
+        back_populates="user",
+        cascade="all, delete",
+        passive_deletes=True, )
+    conversation_state: so.Mapped["ConversationState"] = so.relationship(
+        back_populates="user",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class Context(db.Model):
@@ -32,16 +39,23 @@ class Context(db.Model):
 class ConversationState(db.Model):
     __tablename__ = "state"
     id: so.Mapped[str] = so.mapped_column(sa.String(64), primary_key=True)
-    user_id: so.Mapped[str] = so.mapped_column(sa.ForeignKey(User.id))
+    user_id: so.Mapped[str] = so.mapped_column(sa.ForeignKey(User.id, ondelete="CASCADE"))
     user: so.Mapped["User"] = so.relationship(back_populates="conversation_state")
     interview_completed: so.Mapped[bool] = so.mapped_column(sa.Boolean, default=0)
     current_context: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Context.id), nullable=True)
+    completed_contexts: so.Mapped[List["ConversationCompletedContexts"]] = so.relationship(
+        back_populates="conversation",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class ConversationCompletedContexts(db.Model):
     __tablename__ = "conversation_completed_contexts"
-    conversation_id: so.Mapped["ConversationState"] = so.mapped_column(sa.ForeignKey(ConversationState.id), primary_key=True)
+    conversation_id: so.Mapped["ConversationState"] = so.mapped_column(
+        sa.ForeignKey(ConversationState.id, ondelete="CASCADE"), primary_key=True)
     completed_context_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Context.id), primary_key=True)
+    conversation: so.Mapped["ConversationState"] = so.relationship(back_populates="completed_contexts")
 
 
 class Strategy(db.Model):
@@ -53,7 +67,7 @@ class Strategy(db.Model):
 
 class InterviewAnswer(db.Model):
     id: so.Mapped[str] = so.mapped_column(sa.String(64), primary_key=True)
-    user_id: so.Mapped[str] = so.mapped_column(sa.ForeignKey(User.id))
+    user_id: so.Mapped[str] = so.mapped_column(sa.ForeignKey(User.id, ondelete="CASCADE"))
     user: so.Mapped["User"] = so.relationship(back_populates="interview_answers")
     context: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Context.id))
     strategies: so.Mapped[List[int]] = so.mapped_column(sa.ForeignKey(Strategy.id))
